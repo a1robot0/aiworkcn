1b418196fe168aeb794e6d488e5dcc48036abab8
a1robot <128459746+a1robot0@users.noreply.github.com>
2023-03-31 15:55:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,74 @@
-def factorial(n):
-  if n == 0:
-    return 1
-  else:
-    return n * factorial(n - 1)
-print(factorial(5))
+import pygame
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+ 
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = 50
+HEIGHT = 50
+ 
+# This sets the margin between each cell
+MARGIN = 5
+ 
+# Create a 2 dimensional array. A two dimensional
+# array is simply a list of lists.
+grid = []
+for row in range(3):
+    # Add an empty array that will hold each cell
+    # in this row
+    grid.append([])
+    for column in range(3):
+        grid[row].append(0)  # Append a cell
+ 
+# Initialize pygame
+pygame.init()
+ 
+# Set the HEIGHT and WIDTH of the screen
+WINDOW_SIZE = [255, 255]
+screen = pygame.display.set_mode(WINDOW_SIZE)
+ 
+# Set title of screen
+pygame.display.set_caption("Tic Tac Toe")
+ 
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+ 
+# -------- Main Program Loop -----------
+while not done:
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+ 
+        # User clicks the mouse. Get the position
+        pos = pygame.mouse.get_pos()
+ 
+        # Change the x/y screen coordinates to grid coordinates
+        column = pos[0] // (WIDTH + MARGIN)
+        row = pos[1] // (HEIGHT + MARGIN)
+ 
+        # Set that location to one
+        grid[row][column] = 1
+        print("Click ", pos, "Grid coordinates: ", row, column)
+ 
+    # Set the screen background
+    screen.fill(BLACK)
+ 
+    # Draw the grid
+    for row in range(3):
+        for column in range(3):
+            color = WHITE
+            if grid[row][column] == 1:
+                color = BLACK
+            pygame.draw.rect(screen,
+                             color,
+                             [(MARGIN + WIDTH) * column + MARGIN,
+                              (MARGIN + HEIGHT) * row + MARGIN,
+                              WIDTH,
+                              HEIGHT])
+ 
+    # Limit to 60 frames per second
+    clock.tick(60)

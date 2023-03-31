8865c14ff26e203defe586ee59c93cd58889e867
a1robot <128459746+a1robot0@users.noreply.github.com>
2023-03-31 15:50:57 +0800

    Update main.clj

--- main.clj
+++ main.clj
@@ -1,4 +1,38 @@
-(do
-  (println "What's your name?")
-  (let [name (read-line)]
-    (println (str "Hey, " name))))
+; Generates a GUI calculator with addition, subtraction, multiplication, and division buttons, as well as a text field to display input and output. The calculator is limited to numbers within 20.
+
+(import javax.swing.JFrame)
+(import javax.swing.JPanel)
+(import javax.swing.JButton)
+(import javax.swing.JTextField)
+(import java.awt.BorderLayout)
+(import java.awt.GridLayout)
+
+(defn create-gui []
+  (let [frame (JFrame. "Calculator")
+        panel (JPanel.)]
+
+    (doto panel
+      (.setLayout (GridLayout. 4 4))
+      (.add (JButton. "7"))
+      (.add (JButton. "8"))
+      (.add (JButton. "9"))
+      (.add (JButton. "+"))
+      (.add (JButton. "4"))
+      (.add (JButton. "5"))
+      (.add (JButton. "6"))
+      (.add (JButton. "-"))
+      (.add (JButton. "1"))
+      (.add (JButton. "2"))
+      (.add (JButton. "3"))
+      (.add (JButton. "*"))
+      (.add (JButton. "0"))
+      (.add (JButton. "C"))
+      (.add (JButton. "="))
+      (.add (JButton. "/")))
+    (doto frame
+      (.add (JTextField.))
+      (.add panel)
+      (.setSize 500 300)
+      (.setVisible true))))
+
+(create-gui)
